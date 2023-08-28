e0a7a87a859664c13cb4558dc1ab6874ebd62633
Emilio Morles <emiliomorles>
2023-08-28 06:09:18 -0600

    v.0.1

---
+++ main.py
@@ -0,0 +1,56 @@
+print(r'''
+                ⣀⣤⣴⣶⣾⣿⣿⣿⣿⣷⣶⣦⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀
+        ⠀⠀⠀⠀⠀⣠⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⣄⠀⠀⠀⠀⠀
+        ⠀⠀⠀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣄⠀⠀⠀
+        ⠀⠀⣴⣿⣿⣿⣿⣿⣿⣿⠟⠿⠿⡿⠀⢰⣿⠁⢈⣿⣿⣿⣿⣿⣿⣿⣿⣦⠀⠀
+        ⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣤⣄⠀⠀⠀⠈⠉⠀⠸⠿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀
+        ⢰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡏⠀⠀⢠⣶⣶⣤⡀⠀⠈⢻⣿⣿⣿⣿⣿⣿⣿⡆
+        ⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⠼⣿⣿⡿⠃⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣷
+        ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠀⠀⢀⣀⣀⠀⠀⠀⠀⢴⣿⣿⣿⣿⣿⣿⣿⣿⣿
+        ⢿⣿⣿⣿⣿⣿⣿⣿⢿⣿⠁⠀⠀⣼⣿⣿⣿⣦⠀⠀⠈⢻⣿⣿⣿⣿⣿⣿⣿⡿
+        ⠸⣿⣿⣿⣿⣿⣿⣏⠀⠀⠀⠀⠀⠛⠛⠿⠟⠋⠀⠀⠀⣾⣿⣿⣿⣿⣿⣿⣿⠇
+        ⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⠇⠀⣤⡄⠀⣀⣀⣀⣀⣠⣾⣿⣿⣿⣿⣿⣿⣿⡟⠀
+        ⠀⠀⠻⣿⣿⣿⣿⣿⣿⣿⣄⣰⣿⠁⢀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠀⠀
+        ⠀⠀⠀⠙⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠋⠀⠀⠀
+        ⠀⠀⠀⠀⠀⠙⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠋⠀⠀⠀⠀⠀
+        ⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠻⠿⢿⣿⣿⣿⣿⡿⠿⠟⠛⠉⠀⠀⠀⠀⠀⠀⠀⠀
+                            _.--.
+                        _.-'_:-'||
+                    _.-'_.-::::'||
+               _.-:'_.-::::::'  ||
+             .'`-.-:::::::'     ||
+            /.'`;|:::::::'      ||_
+           ||   ||::::::'     _.;._'-._
+           ||   ||:::::'  _.-!oo @.!-._'-.
+           \'.  ||:::::.-!()oo @!()@.-'_.|
+            '.'-;|:.-'.&$@.& ()$%-'o.'\U||
+              `>'-.!@%()@'@_%-'_.-o _.|'||
+               ||-._'-.@.-'_.-' _.-o  |'||
+               ||=[ '-._.-\\/.-'    o |'||
+               || '-.]=|| |'|      o  |'||
+               ||      || |'|        _| ';
+               ||      || |'|    _.-'_.-'
+               |'-._   || |'|_.-'_.-'
+          bitcoin '-._'-.|| |' `_.-'
+                    '-.||_/.-'
+''')
+
+print("Bienvenido a la jungla perdida de Bitcoin.")
+print("Tu misión es encontrar el tesoro de bitcoin.\n") 
+#diagram = https://app.diagrams.net/?src=about#Hemiliomorles%2Fdiagrams.net%2Fmain%2Fbitcoin_game_diagram.drawio
+choice1 = input('Estás en la jungla de bitcoin ahora. Tu primera aventura te lleva a dos cuevas. \nA tu "izquierda" tienes la cueva de bitcoin core y a tu "derecha" tienes la cueva de bitcoin cash. ¿A cual ingresas?\n').lower()
+if choice1 == "izquierda":
+  # CONTINUE
+  choice2 = input('\nEligió sabiamente. Ahora necesita decidir entre "nadar" por debajo de la cueva a través de la ruta de Satoshi o "saltar" sobre la pared de Vitalik?\n').lower()
+  if choice2 == "nadar":
+    choice3 = input('\n¡Increible!. Ahora estas en el desafio final. Hay cuatro puertas delante de ti, cada una con un color diferente, "blanco", "negro", "naranja" y "negro". ¿Cual puerta vas a abrir?\n').lower()
+    if choice3 == "naranja":
+      print("\n¡Felicitaciones!, Ganaste la busqueda del tesoro bitcoin. \n" + "Ahora ve a bitcoin.org para que puedas encontrar tu camino hacia un bitcoin completo.")
+    else:
+      print("\nLo siento, estás muerto. Debiste abrir la naranja.")  
+  else:
+    print("\nLo siento, estás muerto. Vitalik es un sh*tcoiner.")
+  pass
+else:
+  print("\nLo siento, estás muerto. Bitcoin Cash es una estafa porque está centralizado.")
+
